c50f17249d6005f5cb77c2eca5e414664ff2109b
Tarun <57524319+tarun19991121@users.noreply.github.com>
2024-01-29 20:28:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var image1 = document.querySelectorAll("img")[0];
 var image2 = document.querySelectorAll("img")[1];
 
 image1.setAttribute("src", randomImageSource1);
-image1.setAttribute("src", randomImageSource2);
+image2.setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
